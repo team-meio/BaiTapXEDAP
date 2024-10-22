54cf5f1710902454e2cb707413ce51b7441f410b
team-meio <phamhuyentrang562003@gmail.com>
2024-10-22 22:01:18 +0700

    cau3

--- 21071641-phamhuyentrang/App.js
+++ 21071641-phamhuyentrang/App.js
@@ -1,13 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Screen_01 from "./components/screen_01"
+// import Screen_01 from "./components/screen_01"
+import Screen_02 from "./components/screen_02"
+
 const Stack = createNativeStackNavigator();
 
 export default function App  ()  {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" >
-        <Stack.Screen name="Home" component={Screen_01} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName="ManHinh2" >
+        <Stack.Screen name="ManHinh2" component={Screen_02} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
